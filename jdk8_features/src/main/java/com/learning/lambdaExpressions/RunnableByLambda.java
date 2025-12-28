d2492e4df3b325ff47338795096ef7d93b34d185
Sohrab Alam <sohrab.alam86@gmail.com>
2025-12-28 20:10:09 +0530

    Java8 Lembda Expressions implement Runnable.

---
+++ jdk8_features/src/main/java/com/learning/lambdaExpressions/RunnableByLambda.java
@@ -0,0 +1,23 @@
+package com.learning.lambdaExpressions;
+
+public class RunnableByLambda {
+
+	public static void main(String[] args) {
+
+		Runnable r = () -> {
+			for (int i = 1; i <= 10; i++) {
+				System.out.println("Child thread");
+			}
+		};
+
+		Thread t = new Thread(r);
+
+		t.start();
+
+		for (int i = 1; i <= 10; i++) {
+			System.out.println("Main Thread");
+		}
+
+	}
+
+}
