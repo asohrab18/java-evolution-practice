457d294010e53000c5c5c4530f23ee0a16f979e3
Sohrab Alam <sohrab.alam86@gmail.com>
2025-12-28 20:45:16 +0530

    Customized Functional Interface.

---
+++ jdk8_features/src/main/java/com/learning/functionalInterfaces/Demo.java
@@ -0,0 +1,27 @@
+package com.learning.functionalInterfaces;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		TaxCalculator gstCalc = amount -> amount * 18 / 100;
+		TaxCalculator serviceTaxCalc = amount -> amount * 15 / 100;
+
+		double basicPrice = 160000d;
+
+		double gst = gstCalc.calculate(basicPrice);
+		double serviceTax = serviceTaxCalc.calculate(basicPrice);
+		double totalTax = gst + serviceTax;
+
+		double totalAmount = basicPrice + totalTax;
+
+		System.out.println("Bacic price = INR " + basicPrice);
+		
+		System.out.println("18% GST = INR " + gst);
+		System.out.println("15% Service Tax = INR " + serviceTax);
+		System.out.println("Total Tax = INR " + totalTax);
+		
+		System.out.println("Total Price (Bacic price + GST + Service Tax) = INR " + totalAmount);
+		
+	}
+
+}
---
+++ jdk8_features/src/main/java/com/learning/functionalInterfaces/TaxCalculator.java
@@ -0,0 +1,6 @@
+package com.learning.functionalInterfaces;
+
+@FunctionalInterface
+public interface TaxCalculator {
+	double calculate(double amount);
+}
