7e51a4f2eeaa830706a773c4bd9aacf5b7f2cf30
Sohrab Alam <sohrab.alam86@gmail.com>
2025-12-31 15:33:31 +0530

    Basic Functional Interfaces & Lambda Expressions.

--- jdk8_features/src/main/java/com/learning/functionalInterfaces/consumers/ConsumerDemo.java
+++ jdk8_features/src/main/java/com/learning/functionalInterfaces/consumers/ConsumerDemo.java
@@ -5,21 +5,24 @@ import java.util.function.Consumer;
 
 public class ConsumerDemo {
 
+	static Consumer<String> loggerConsumer = s -> System.out.println("\n[INFO] " + s);
+
 	static Consumer<String> printConsumer = s -> System.out.println(s);
 
 	public static void printMessage(String message) {
+		loggerConsumer.accept("Inside printMessage:");
 		printConsumer.accept(message);
 	}
 
 	public static void printItems() {
-		System.out.println("\nConsumer used to print List items:");
+		loggerConsumer.accept("Consumer used to print List items:");
 		List<String> fruits = List.of("Apple", "Banana", "Coconut", "Guava");
 		fruits.forEach(printConsumer);
 
 	}
 
 	public static void useInStream() {
-		System.out.println("\nConsumer used in Stream:");
+		loggerConsumer.accept("Consumer used in Stream:");
 		List<String> fruits = List.of("Apple", "Banana", "Coconut", "Guava");
 		fruits.stream().filter(s -> s.length() < 6).forEach(printConsumer);
 	}
