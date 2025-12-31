2e1f1864f26da0d2dfd15ba4becbda5c3e5f481a
Sohrab Alam <sohrab.alam86@gmail.com>
2025-12-31 19:05:07 +0530

    Basic Functional Interfaces & Lambda Expressions.

--- jdk8_features/src/main/java/com/learning/functionalInterfaces/consumers/ConsumerDemo.java
+++ jdk8_features/src/main/java/com/learning/functionalInterfaces/consumers/ConsumerDemo.java
@@ -2,12 +2,25 @@ package com.learning.functionalInterfaces.consumers;
 
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.DoubleConsumer;
+import java.util.function.IntConsumer;
+import java.util.function.LongConsumer;
 
 public class ConsumerDemo {
 
 	static Consumer<String> loggerConsumer = s -> System.out.println("\n[INFO] " + s);
 
 	static Consumer<String> printConsumer = s -> System.out.println(s);
+
+	static DoubleConsumer printRupeesInDollarConsumer = r -> System.out
+			.println("INR " + r + " = USD " + (r * 0.01113d));
+
+	static DoubleConsumer printDollarInRupeesConsumer = r -> System.out
+			.println("USD " + r + " = INR " + (r / 0.01113d));
+
+	static LongConsumer printRupeesInPaisaConsumer = r -> System.out.println("INR " + r + " = " + (r * 100) + " paisa");
+
+	static IntConsumer printDollarInCentsConsumer = r -> System.out.println("USD " + r + " = " + (r * 100) + " cents");
 
 	public static void printMessage(String message) {
 		loggerConsumer.accept("Inside printMessage:");
@@ -27,10 +40,26 @@ public class ConsumerDemo {
 		fruits.stream().filter(s -> s.length() < 6).forEach(printConsumer);
 	}
 
+	public static void convertCurrency() {
+		loggerConsumer.accept("Currency Conversion:");
+		double rupees = 1000d;
+		printRupeesInDollarConsumer.accept(rupees);
+
+		double dollar = 1d;
+		printDollarInRupeesConsumer.accept(dollar);
+
+		long rupeesL = 1l;
+		printRupeesInPaisaConsumer.accept(rupeesL);
+
+		int dollarI = 1;
+		printDollarInCentsConsumer.accept(dollarI);
+	}
+
 	public static void main(String[] args) {
 		printMessage("Hi");
 		printItems();
 		useInStream();
+		convertCurrency();
 	}
 
 }
