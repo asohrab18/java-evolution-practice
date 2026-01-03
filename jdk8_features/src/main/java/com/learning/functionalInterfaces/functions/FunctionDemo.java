fa27e9f08f834bedf553c2423fc2e90481ebaf51
Sohrab Alam <sohrab.alam86@gmail.com>
2026-01-03 14:30:17 +0530

    Basic Functional Interfaces & Lambda Expressions.

--- jdk8_features/src/main/java/com/learning/functionalInterfaces/functions/FunctionDemo.java
+++ jdk8_features/src/main/java/com/learning/functionalInterfaces/functions/FunctionDemo.java
@@ -2,7 +2,16 @@ package com.learning.functionalInterfaces.functions;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.DoubleFunction;
+import java.util.function.DoubleToIntFunction;
+import java.util.function.DoubleToLongFunction;
 import java.util.function.Function;
+import java.util.function.IntFunction;
+import java.util.function.IntToDoubleFunction;
+import java.util.function.IntToLongFunction;
+import java.util.function.LongFunction;
+import java.util.function.LongToDoubleFunction;
+import java.util.function.LongToIntFunction;
 
 public class FunctionDemo {
 
@@ -58,6 +67,131 @@ public class FunctionDemo {
 	// Function.identity() – Returns the Input as-is. Useful in Streams.
 	static Function<String, String> identityFunction = Function.identity();
 
+	static DoubleFunction<String> designationFunction = salary -> {
+		if (salary <= 0d) {
+			return "NONE";
+		} else if (salary >= 5000d && salary <= 30000d) {
+			return "Software Engineer Trainee";
+		} else if (salary > 30000d && salary <= 40000d) {
+			return "Software Engineer";
+		} else if (salary > 40000d && salary <= 50000d) {
+			return "Senior Software Engineer";
+		} else if (salary > 50000d && salary <= 100000d) {
+			return "Team Lead";
+		} else if (salary > 100000d && salary <= 300000d) {
+			return "Manager";
+		}
+		return "OTHERS";
+	};
+
+	static DoubleToIntFunction cgpaFunction = marks -> {
+		int cgpa = 0;
+		if (marks > 0d && marks <= 30d) {
+			cgpa = 1;
+		} else if (marks > 30d && marks <= 50d) {
+			cgpa = 2;
+		} else if (marks > 50d && marks <= 70d) {
+			cgpa = 3;
+		} else if (marks > 70d && marks <= 80d) {
+			cgpa = 4;
+		} else if (marks > 80d && marks <= 100d) {
+			cgpa = 5;
+		}
+		return cgpa;
+	};
+
+	static DoubleToLongFunction roundFunction = value -> Math.round(value);
+
+	static IntFunction<String> remarksFunction = rank -> {
+		String remarks = "POOR PERFORMANCE!";
+		if (rank <= 0) {
+			remarks = "FAILED";
+		} else if (rank == 1) {
+			remarks = "EXCELLENT PERFORMANCE!";
+		} else if (rank == 2) {
+			remarks = "BETTER PERFORMANCE!";
+		} else if (rank == 3) {
+			remarks = "GOOD PERFORMANCE!";
+		} else if (rank == 4) {
+			remarks = "PERFORMANCE IS FINE.";
+		} else if (rank == 5) {
+			remarks = "WORK HARD";
+		}
+		return remarks;
+	};
+
+	static IntToDoubleFunction factorialFunction = num -> {
+		double fact = 1d;
+		if (num < 0) {
+			return 0d;
+		}
+		if (num == 0) {
+			return fact;
+		}
+		for (int i = num; i > 0; i--) {
+			fact = fact * i;
+		}
+		return fact;
+	};
+
+	static IntToLongFunction squareLongFunction = num -> {
+		return (long) num * num;
+	};
+
+	static LongFunction<String> nameOfIdFunction = id -> {
+		String name = "ANONYMOUS";
+		if (id == 1000000000l) {
+			name = "ADAM GILCHRIST";
+		} else if (id == 2000000000l) {
+			name = "JESICA FERNANDEZ";
+		} else if (id == 3000000000l) {
+			name = "KELVIN SMITH";
+		} else if (id == 4000000000l) {
+			name = "MARCO POLO";
+		} else if (id == 5000000000l) {
+			name = "NICK JONAS";
+		} else if (id == 6000000000l) {
+			name = "PATRICK JOHNSON";
+		}
+		return name;
+	};
+
+	static LongToDoubleFunction marksOfIdFunction = id -> {
+		double marks = 0d;
+		if (id == 1000000000l) {
+			marks = 90.50d;
+		} else if (id == 2000000000l) {
+			marks = 70.25d;
+		} else if (id == 3000000000l) {
+			marks = 60.75d;
+		} else if (id == 4000000000l) {
+			marks = 40.25d;
+		} else if (id == 5000000000l) {
+			marks = 66.50d;
+		} else if (id == 6000000000l) {
+			marks = 80.75d;
+		}
+		return marks;
+	};
+
+	static LongToIntFunction rankOfIdFunction = id -> {
+		int rank = 0;
+		if (id == 1000000000l) {
+			rank = 1;
+		} else if (id == 2000000000l) {
+			rank = 2;
+		} else if (id == 3000000000l) {
+			rank = 3;
+		} else if (id == 4000000000l) {
+			rank = 4;
+		} else if (id == 5000000000l) {
+			rank = 5;
+		} else if (id == 6000000000l) {
+			rank = 6;
+		}
+		return rank;
+	};
+
 	public static void testFunctions() {
 		String input = "ABCD";
 		Integer strLength = strLengthFunction.apply(input);
@@ -65,7 +199,7 @@ public class FunctionDemo {
 
 		System.out.println("Identity Function O/P = " + identityFunction.apply(input) + "\n");
 
-		Integer num = 2;
+		Integer num = 3;
 		Integer squareAndThenCube = squareFunction.andThen(cubeFunction).apply(num);
 		System.out.println("Number = " + num + "\nIt's square and then cube = " + squareAndThenCube + "\n");
 
@@ -92,6 +226,37 @@ public class FunctionDemo {
 		 * 
 		 */
 
+		double salary = 60000.50d;
+		String designation = designationFunction.apply(salary);
+		System.out.println("Salary: INR " + salary + "\nDesignation: " + designation + "\n");
+
+		double marks = 78.5d;
+		int cgpa = cgpaFunction.applyAsInt(marks);
+		System.out.println("Marks = " + marks + "\nCGPA = " + cgpa + "\n");
+
+		long roundedMarks = roundFunction.applyAsLong(marks);
+		System.out.println("Marks = " + marks + "\nRounded Marks = " + roundedMarks + "\n");
+
+		int rank = 2;
+		String remarks = remarksFunction.apply(rank);
+		System.out.println("rank = " + rank + "\nRemarks: " + remarks + "\n");
+
+		double factorial = factorialFunction.applyAsDouble(num);
+		System.out.println("num = " + num + "\nFactorial = " + factorial + "\n");
+
+		int number = 1234567891;
+		long square = squareLongFunction.applyAsLong(number);
+		System.out.println("number = " + number + "\nSquare of number = " + square + "\n");
+
+		long candidateId = 2000000000l;
+		String candidateName = nameOfIdFunction.apply(candidateId);
+		System.out.println("Candidate Id = " + candidateId + "\nName = " + candidateName);
+
+		double marksOfId = marksOfIdFunction.applyAsDouble(candidateId);
+		System.out.println("Marks = " + marksOfId);
+
+		int rankOfId = rankOfIdFunction.applyAsInt(candidateId);
+		System.out.println("Rank = " + rankOfId + "\n");
 	}
 
 	public static void testFunctionsUsingStream() {
