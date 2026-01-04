0a01f0901ee9358333e19d69674bdc3482da822e
Sohrab Alam <sohrab.alam86@gmail.com>
2026-01-04 19:11:03 +0530

    Basic Functional Interfaces & Lambda Expressions.

--- jdk8_features/src/main/java/com/learning/doublecolon/operator/MethodReferenceDemo.java
+++ jdk8_features/src/main/java/com/learning/doublecolon/operator/MethodReferenceDemo.java
@@ -1,11 +1,27 @@
 package com.learning.doublecolon.operator;
 
+import com.learning.functionalInterfaces.customized.Greeting;
+import com.learning.model.EmployeeDto;
+
 public class MethodReferenceDemo {
 
 	static Runnable firstRunnable = Test::show;
 
 	static Test test = new Test();
 	static Runnable secondRunnable = test::display;
+
+	static Runnable thirdRunnable = EmployeeDto::printEmployees;
+
+	static Greeting greeting = EmployeeDto::printEmployees;
+
+	public static void testEmployees() {
+		Thread t = new Thread(thirdRunnable);
+		t.start();
+
+		for (int i = 1; i <= 3; i++) {
+			System.out.println(i + ": Main-Thread");
+		}
+	}
 
 	public static void testShow() {
 		Thread t = new Thread(firstRunnable);
@@ -25,9 +41,15 @@ public class MethodReferenceDemo {
 		}
 	}
 
+	public static void testGreeting() {
+		greeting.perform();
+	}
+
 	public static void main(String[] args) {
 		testShow();
 		testDisplay();
+		testEmployees();
+		testGreeting();
 	}
 
 }
--- jdk8_features/src/main/java/com/learning/model/EmployeeDto.java
+++ jdk8_features/src/main/java/com/learning/model/EmployeeDto.java
@@ -23,4 +23,9 @@ public class EmployeeDto {
 
 		return List.of(e1, e2, e3, e4, e5, e6, e7, e8, e9, e10, e11, e12, e13, e14, e15);
 	}
+
+	public static void printEmployees() {
+		List<Employee> employees = findEmployees();
+		System.out.println(employees);
+	}
 }
