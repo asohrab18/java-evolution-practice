038eac0a15777820ae45902a51039507f92a9c32
Sohrab Alam <sohrab.alam86@gmail.com>
2026-01-07 20:30:49 +0530

    Stream methods.

--- jdk8_features/src/main/java/com/learning/streams/CollectorsDemo.java
+++ jdk8_features/src/main/java/com/learning/streams/CollectorsDemo.java
@@ -1,7 +1,9 @@
 package com.learning.streams;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 import com.learning.model.Employee;
@@ -12,11 +14,13 @@ import com.learning.model.StudentDto;
 public class CollectorsDemo {
 
 	private static List<Employee> employees = EmployeeDto.findEmployees();
+	private static List<Student> students = StudentDto.findStudents();
 
 	static void printEmployeesByDepartment() {
 		Map<String, List<Employee>> byDept = employees.stream().collect(Collectors.groupingBy(Employee::getDept));
 
 		System.out.println(byDept);
+		System.out.println("=======================================================");
 	}
 
 	/**
@@ -24,7 +28,6 @@ public class CollectorsDemo {
 	 * mean (average). Finally it returns a Double.
 	 */
 	static void testAveragingDouble() {
-		System.out.println("=======================================================");
 		List<Integer> numbers = List.of(1, 2, 3);
 		System.out.println("Numbers = " + numbers);
 		Double average = numbers.stream().collect(Collectors.averagingDouble(n -> n));
@@ -37,7 +40,6 @@ public class CollectorsDemo {
 		System.out.println("\nAverage Salary = " + averageSalary);
 		System.out.println("=======================================================");
 
-		List<Student> students = StudentDto.findStudents();
 		List<Integer> ages = students.stream().map(s -> s.getAge()).collect(Collectors.toList());
 		System.out.println("Ages of students = " + ages);
 		Double averageAge = students.stream().collect(Collectors.averagingDouble(Student::getAge));
@@ -54,10 +56,82 @@ public class CollectorsDemo {
 		System.out.println("=======================================================");
 	}
 
+	static void testAveragingInt() {
+		List<Integer> numbers = List.of(1, 2, 3);
+		System.out.println("Numbers = " + numbers);
+		Double average = numbers.stream().collect(Collectors.averagingInt(n -> n));
+		System.out.println("Average = " + average);
+		System.out.println("=======================================================");
+
+		List<Integer> ages = employees.stream().map(e -> e.getAge()).collect(Collectors.toList());
+		System.out.println("Ages of employees = " + ages);
+		Double averageAge = employees.stream().collect(Collectors.averagingInt(Employee::getAge));
+		System.out.println("\nAverage Age = " + averageAge);
+		System.out.println("=======================================================");
+	}
+
+	static void testAveragingLong() {
+		List<Long> numbers = List.of(123456L, 2765432L, 3982376L);
+		System.out.println("Numbers = " + numbers);
+		Double average = numbers.stream().collect(Collectors.averagingLong(n -> n));
+		System.out.println("Average = " + average);
+		System.out.println("=======================================================");
+	}
+
+	static void testCollectingAndThen() {
+		List<String> mutableNames = students.stream().map(s -> s.getName()).collect(Collectors.toList());
+		System.out.println("Mutable Names = " + mutableNames);
+		mutableNames.add("Zeeshan");
+		System.out.println("\nUpdated Mutable Names = " + mutableNames);
+
+		System.out.println("=======================================================");
+		List<String> immutableNames = students.stream().map(s -> s.getName())
+				.collect(Collectors.collectingAndThen(Collectors.toList(), Collections::unmodifiableList));
+
+		System.out.println("Immutable Names = " + immutableNames);
+		try {
+			immutableNames.add("Mike");
+		} catch (UnsupportedOperationException e) {
+			System.out.println("\nCannot modify immutable Names = " + immutableNames);
+		}
+		System.out.println("=======================================================");
+
+		List<Integer> numbers = List.of(5, 5, 5, 1, 1, 1, 4, 4, 4, 2, 2, 2, 3, 3);
+		System.out.println("Numbers = " + numbers);
+
+		int howManyUniqueNumbers = numbers.stream()
+				.collect(Collectors.collectingAndThen(Collectors.toSet(), Set::size));
+
+		System.out.println("How many unique Numbers are available?\nANS: " + howManyUniqueNumbers);
+		System.out.println("=======================================================");
+
+		List<Integer> sortedNumbers = numbers.stream()
+				.collect(Collectors.collectingAndThen(Collectors.toSet(), s -> s.stream().sorted().toList()));
+
+		System.out.println("Sorted unique Numbers = " + sortedNumbers);
+		System.out.println("=======================================================");
+	}
+
+	static void testCounting() {
+		List<Integer> ages = employees.stream().map(e -> e.getAge())
+				.collect(Collectors.collectingAndThen(Collectors.toList(), list -> list.stream().sorted().toList()));
+		System.out.println("Ages = " + ages);
+
+		Long minors = employees.stream().filter(e -> e.getAge() < 18).collect(Collectors.counting());
+		System.out.println("Minors = " + minors);
+
+		Long adults = employees.stream().filter(e -> e.getAge() >= 18).collect(Collectors.counting());
+		System.out.println("Adults = " + adults);
+		System.out.println("=======================================================");
+	}
+
 	public static void main(String[] args) {
 		printEmployeesByDepartment();
 		testAveragingDouble();
-
+		testAveragingInt();
+		testAveragingLong();
+		testCollectingAndThen();
+		testCounting();
 	}
 
 }
