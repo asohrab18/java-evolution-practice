ec5ab82e9e186281e0e54b45f0c6844d7f886d69
Sohrab Alam <sohrab.alam86@gmail.com>
2026-01-27 12:27:32 +0530

    Stream methods.

--- jdk8_features/src/main/java/com/learning/model/AppUtils.java
+++ jdk8_features/src/main/java/com/learning/model/AppUtils.java
@@ -9,40 +9,69 @@ public final class AppUtils {
 	private AppUtils() {
 	}
 
+	/**
+	 * ============== List of some custom classes: ==============
+	 */
 	public static final List<Candidate> CANDIDATES = CandidateDto.findCandidates();
 
-	public static final List<Integer> DUPLICATE_NUMBERS = List.of(5, 5, 5, 1, 1, 1, 4, 4, 4, 2, 2, 2, 3, 3);
-
-	public static final List<Integer> EVEN_NUMBERS = List.of(2, 4, 6, 8, 9);
-
 	public static final List<Employee> EMPLOYEES = EmployeeDto.findEmployees();
-
-	public static final List<String> NAMES = List.of("Ali", "Adam", "Eves", "Celina", "Denis", "Menis");
-
-	public static final List<Integer> NUMBERS = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11);
-
-	public static final List<Integer> NUMBERS_INCLUDING_NULL = Arrays.asList(1, 2, null, 3, 4, 5, 6, null, 7, 8);
-
-	public static final List<Long> NUMBERS_L = List.of(123456L, 2765432L, 3982376L);
-
-	public static final List<Integer> ODD_NUMBERS = List.of(1, 3, 5, 7, 9);
 
 	public static final List<Order> ORDERS = OrderDto.findOrders();
 
 	public static final List<Person> PERSONS = PersonDto.findPersons();
 
+	public static final List<Student> STUDENTS = StudentDto.findStudents();
+
+	/**
+	 * ============== List of some data types: ===============
+	 */
+
+	public static final List<Integer> DUPLICATE_NUMBERS = List.of(5, 5, 5, 1, 1, 1, 4, 4, 4, 2, 2, 2, 3, 3);
+
+	public static final List<Integer> EVEN_NUMBERS = List.of(2, 4, 6, 8, 9);
+
+	public static final List<Integer> NUMBERS = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11);
+
+	public static final List<Integer> NUMBERS_INCLUDING_NULL = Arrays.asList(1, 2, null, 3, 4, 5, 6, null, 7, 8);
+
+	public static final List<Integer> ODD_NUMBERS = List.of(1, 3, 5, 7, 9);
+
+	public static final List<Long> NUMBERS_L = List.of(123456L, 2765432L, 3982376L);
+
 	public static final List<Double> PRICES = List.of(100.18d, 200.86d, 300.56d);
 
-	public static final List<Student> STUDENTS = StudentDto.findStudents();
+	public static final List<String> NAMES = List.of("Ali", "Adam", "Eves", "Celina", "Denis", "Menis");
 
 	public static final List<String> WORDS = List.of("Apple", "Coconut", "Spoon", "Table", "Coconut", "Microservices");
 
+	/**
+	 * =============== Some methods are below: ================
+	 */
 	public static <T> Stream<T> getEmptyStream() {
 		return Stream.empty();
 	}
 
 	/** We return Stream.empty(); to avoid NullPointerException. */
-	public static Stream<String> getStream(String key) {
+	public static Stream<Integer> getIntStream(String key) {
+		Stream<Integer> resultStream = Stream.empty();
+		if (key == null || key.isBlank() || key.isEmpty()) {
+			return resultStream;
+		}
+		if (key.equalsIgnoreCase("DUPLICATE_NUMBERS")) {
+			resultStream = DUPLICATE_NUMBERS.stream();
+		} else if (key.equalsIgnoreCase("EVEN_NUMBERS")) {
+			resultStream = EVEN_NUMBERS.stream();
+		} else if (key.equalsIgnoreCase("NUMBERS")) {
+			resultStream = NUMBERS.stream();
+		} else if (key.equalsIgnoreCase("NUMBERS_INCLUDING_NULL")) {
+			resultStream = NUMBERS_INCLUDING_NULL.stream();
+		} else if (key.equalsIgnoreCase("ODD_NUMBERS")) {
+			resultStream = ODD_NUMBERS.stream();
+		}
+		return resultStream;
+	}
+
+	public static Stream<String> getStringStream(String key) {
 		Stream<String> resultStream = Stream.empty();
 		if (key == null || key.isBlank() || key.isEmpty()) {
 			return resultStream;
@@ -51,25 +80,6 @@ public final class AppUtils {
 			resultStream = WORDS.stream();
 		} else if (key.equalsIgnoreCase("NAMES")) {
 			resultStream = NAMES.stream();
-		}
-		return resultStream;
-	}
-
-	public static Stream<Integer> getIntStream(String key) {
-		Stream<Integer> resultStream = Stream.empty();
-		if (key == null || key.isBlank() || key.isEmpty()) {
-			return resultStream;
-		}
-		if (key.equalsIgnoreCase("DUPLICATE")) {
-			resultStream = DUPLICATE_NUMBERS.stream();
-		} else if (key.equalsIgnoreCase("EVEN")) {
-			resultStream = EVEN_NUMBERS.stream();
-		} else if (key.equalsIgnoreCase("NUMBERS")) {
-			resultStream = NUMBERS.stream();
-		} else if (key.equalsIgnoreCase("NUMBERS_INCLUDING_NULL")) {
-			resultStream = NUMBERS_INCLUDING_NULL.stream();
-		} else if (key.equalsIgnoreCase("ODD")) {
-			resultStream = ODD_NUMBERS.stream();
 		}
 		return resultStream;
 	}
--- jdk8_features/src/main/java/com/learning/streams/StreamDemo.java
+++ jdk8_features/src/main/java/com/learning/streams/StreamDemo.java
@@ -65,87 +65,75 @@ public class StreamDemo {
 
 	public static void testAllMatch() {
 		System.out.println("\n\n===================== testAllMatch() =====================");
-		List<Integer> numbers1 = AppUtils.ODD_NUMBERS;
-		List<Integer> numbers2 = AppUtils.EVEN_NUMBERS;
-		List<String> NAMES = AppUtils.NAMES;
 
-		Stream<Integer> numbersStream = numbers1.stream();
+		Stream<Integer> numbersStream = AppUtils.getIntStream("ODD_NUMBERS");
 		boolean allOdd = numbersStream.allMatch(i -> i % 2 != 0);
 
-		System.out.println("Numbers1 = " + numbers1);
 		System.out.println("Are all NUMBERS Odd? ANS: " + allOdd);
 		System.out.println("------------------------------------------------------------");
 
-		Stream<Integer> numbersStream2 = numbers2.stream();
+		Stream<Integer> numbersStream2 = AppUtils.getIntStream("EVEN_NUMBERS");
 		boolean allEven = numbersStream2.allMatch(i -> i % 2 == 0);
 
-		System.out.println("Numbers2 = " + numbers2);
 		System.out.println("Are all NUMBERS Even? ANS: " + allEven);
 		System.out.println("------------------------------------------------------------");
 
-		Stream<String> namesStream = NAMES.stream();
+		Stream<String> namesStream = AppUtils.getStringStream("NAMES");
 		boolean allNamesStartWithA = namesStream.allMatch(s -> s.startsWith("A"));
 
-		System.out.println("Names = " + NAMES);
 		System.out.println("Do all NAMES start with A? ANS: " + allNamesStartWithA);
 		System.out.println("------------------------------------------------------------");
 	}
 
 	public static void testAnyMatch() {
 		System.out.println("\n\n===================== testAnyMatch() =====================");
-		List<Integer> numbers1 = AppUtils.NUMBERS;
-		List<Integer> numbers2 = AppUtils.EVEN_NUMBERS;
-		List<String> NAMES = AppUtils.NAMES;
 
-		Stream<Integer> numbersStream = numbers1.stream();
+		Stream<Integer> numbersStream = AppUtils.getIntStream("NUMBERS");
 		boolean anyEven = numbersStream.anyMatch(i -> i % 2 == 0);
 
-		System.out.println("Numbers1 = " + numbers1);
 		System.out.println("Is there any even number? ANS: " + anyEven);
 		System.out.println("------------------------------------------------------------");
 
-		Stream<Integer> numbersStream2 = numbers2.stream();
+		Stream<Integer> numbersStream2 = AppUtils.getIntStream("EVEN_NUMBERS");
 		boolean anyOdd = numbersStream2.anyMatch(i -> i % 2 != 0);
-		System.out.println("Numbers2 = " + numbers2);
 		System.out.println("Is there any odd number? ANS: " + anyOdd);
 		System.out.println("------------------------------------------------------------");
 
-		Stream<String> namesStream = NAMES.stream();
+		Stream<String> namesStream = AppUtils.getStringStream("NAMES");
 		boolean anyNameStartsWithA = namesStream.anyMatch(s -> s.startsWith("A"));
 
-		System.out.println("Names = " + NAMES);
 		System.out.println("Is there any name starting with A? ANS: " + anyNameStartsWithA);
 		System.out.println("------------------------------------------------------------");
 	}
 
 	public static void testCollect_V1() {
 		System.out.println("\n\n===================== testCollect_V1() =====================");
-		List<Integer> NUMBERS = AppUtils.NUMBERS;
-		List<Integer> EVEN_NUMBERS = NUMBERS.stream().filter(n -> n % 2 == 0).collect(Collectors.toList());
-		System.out.println("Numbers: " + NUMBERS);
+		List<Integer> EVEN_NUMBERS = AppUtils.getIntStream("NUMBERS").filter(n -> n % 2 == 0)
+				.collect(Collectors.toList());
+
 		System.out.println("Even NUMBERS: " + EVEN_NUMBERS);
 		System.out.println("------------------------------------------------------------");
 
-		List<Integer> numbers2 = AppUtils.DUPLICATE_NUMBERS;
-		Set<Integer> uniqueNumbers = numbers2.stream().collect(Collectors.toSet());
-		System.out.println("Numbers: " + numbers2);
+		Set<Integer> uniqueNumbers = AppUtils.getIntStream("DUPLICATE_NUMBERS").collect(Collectors.toSet());
+
 		System.out.println("unique NUMBERS: " + uniqueNumbers);
 		System.out.println("------------------------------------------------------------");
 	}
 
 	public static void testCollect_V2() {
 		System.out.println("\n\n===================== testCollect_V2() =====================");
-		List<String> words = AppUtils.WORDS.stream().collect(ArrayList::new, ArrayList::add, ArrayList::addAll);
+		List<String> words = AppUtils.getStringStream("WORDS").collect(ArrayList::new, ArrayList::add, ArrayList::addAll);
 
 		System.out.println("words: " + words);
 		System.out.println("------------------------------------------------------------");
 
-		Set<Integer> numbers = AppUtils.DUPLICATE_NUMBERS.stream().collect(HashSet::new, HashSet::add, HashSet::addAll);
+		Set<Integer> numbers = AppUtils.getIntStream("DUPLICATE_NUMBERS").collect(HashSet::new, HashSet::add,
+				HashSet::addAll);
 
 		System.out.println("numbers: " + numbers);
 		System.out.println("------------------------------------------------------------");
 
-		StringBuilder nameBuilder = AppUtils.NAMES.stream().collect(StringBuilder::new, StringBuilder::append,
+		StringBuilder nameBuilder = AppUtils.getStringStream("NAMES").collect(StringBuilder::new, StringBuilder::append,
 				StringBuilder::append);
 
 		System.out.println("nameBuilder: " + nameBuilder);
@@ -157,7 +145,8 @@ public class StreamDemo {
 			}
 		};
 
-		List<Integer> evenNumbers = AppUtils.NUMBERS.stream().collect(ArrayList::new, accumulator, ArrayList::addAll);
+		List<Integer> evenNumbers = AppUtils.getIntStream("NUMBERS").collect(ArrayList::new, accumulator,
+				ArrayList::addAll);
 
 		System.out.println("evenNumbers: " + evenNumbers);
 		System.out.println("------------------------------------------------------------");
@@ -165,8 +154,8 @@ public class StreamDemo {
 
 	public static void testConcat() {
 		System.out.println("\n\n===================== testConcat() =====================");
-		Stream<String> wordsStream = AppUtils.WORDS.stream();
-		Stream<String> namesStream = AppUtils.NAMES.stream();
+		Stream<String> wordsStream = AppUtils.getStringStream("WORDS");
+		Stream<String> namesStream = AppUtils.getStringStream("NAMES");
 
 		Stream<String> concatedStream = Stream.concat(wordsStream, namesStream);
 		concatedStream.forEach(System.out::println);
@@ -178,16 +167,16 @@ public class StreamDemo {
 		animals.forEach(a -> System.out.println(a.getName()));
 		System.out.println("------------------------------------------------------------");
 
-		Stream<Integer> oddNumbersStream = AppUtils.ODD_NUMBERS.stream();
-		Stream<Integer> evenNumbersStream = AppUtils.EVEN_NUMBERS.stream();
+		Stream<Integer> oddNumbersStream = AppUtils.getIntStream("ODD_NUMBERS");
+		Stream<Integer> evenNumbersStream = AppUtils.getIntStream("EVEN_NUMBERS");
 
 		Stream<Integer> oddEvenconcatedStream = Stream.concat(oddNumbersStream, evenNumbersStream);
 
 		oddEvenconcatedStream.forEach(System.out::println);
 		System.out.println("------------------------------------------------------------");
 
-		Stream<Integer> parallelStream = AppUtils.NUMBERS.stream().parallel();
-		Stream<Integer> normalStream = AppUtils.DUPLICATE_NUMBERS.stream();
+		Stream<Integer> parallelStream = AppUtils.getIntStream("NUMBERS").parallel();
+		Stream<Integer> normalStream = AppUtils.getIntStream("DUPLICATE_NUMBERS");
 
 		Stream<Integer> resultantStream = Stream.concat(parallelStream, normalStream);
 
@@ -212,15 +201,17 @@ public class StreamDemo {
 
 	public static void testDistinct() {
 		System.out.println("\n\n===================== testDistinct() =====================");
-		Stream<Integer> distinctNumbers = AppUtils.DUPLICATE_NUMBERS.stream().distinct();
+		Stream<Integer> distinctNumbers = AppUtils.getIntStream("DUPLICATE_NUMBERS").distinct();
+
 		distinctNumbers.forEach(System.out::println);
 		System.out.println("------------------------------------------------------------");
 
-		Stream<Integer> distinctParallelNumbers = AppUtils.DUPLICATE_NUMBERS.stream().parallel().distinct();
+		Stream<Integer> distinctParallelNumbers = AppUtils.getIntStream("DUPLICATE_NUMBERS").parallel().distinct();
+
 		distinctParallelNumbers.forEach(System.out::println);
 		System.out.println("------------------------------------------------------------");
 
-		Stream<String> distinctWords = AppUtils.WORDS.stream().distinct();
+		Stream<String> distinctWords = AppUtils.getStringStream("WORDS").distinct();
 		distinctWords.forEach(System.out::println);
 		System.out.println("------------------------------------------------------------");
 
@@ -249,18 +240,18 @@ public class StreamDemo {
 		System.out.println("String Stream count = " + strStream.count());
 		System.out.println("------------------------------------------------------------");
 
-		Stream<String> dataStream = AppUtils.getStream("OTHER");
+		Stream<String> dataStream = AppUtils.getStringStream("OTHER");
 		System.out.println("String Stream count = " + dataStream.count());
 		System.out.println("------------------------------------------------------------");
 	}
 
 	public static void testFilter() {
 		System.out.println("\n\n===================== testFilter() =====================");
-		AppUtils.NUMBERS.stream().filter(num -> num % 2 == 0).forEach(System.out::println);
+		AppUtils.getIntStream("NUMBERS").filter(num -> num % 2 == 0).forEach(System.out::println);
 
 		System.out.println("------------------------------------------------------------");
 
-		AppUtils.NAMES.stream().filter(name -> name.startsWith("A")).forEach(System.out::println);
+		AppUtils.getStringStream("NAMES").filter(name -> name.startsWith("A")).forEach(System.out::println);
 
 		System.out.println("------------------------------------------------------------");
 
@@ -270,7 +261,7 @@ public class StreamDemo {
 		System.out
 				.println("\n----------------------- filter & map together below -------------------------------------");
 
-		AppUtils.getStream("WORDS").filter(word -> word.length() <= 5).map(String::toUpperCase)
+		AppUtils.getStringStream("WORDS").filter(word -> word.length() <= 5).map(String::toUpperCase)
 				.forEach(System.out::println);
 
 		System.out.println("------------------------------------------------------------");
@@ -278,7 +269,7 @@ public class StreamDemo {
 
 	public static void testFindAny() {
 		System.out.println("\n\n===================== testFindAny() =====================");
-		Optional<String> wordsOpt = AppUtils.getStream("WORDS").findAny();
+		Optional<String> wordsOpt = AppUtils.getStringStream("WORDS").findAny();
 
 		wordsOpt.ifPresent(System.out::println);
 		System.out.println("------------------------------------------------------------");
