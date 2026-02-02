28fc6fcb6755330b46d0c0ae4f70786b9dbedf6c
Sohrab Alam <sohrab.alam86@gmail.com>
2026-02-03 00:41:11 +0530

    Stream methods.

--- jdk8_features/src/main/java/com/learning/functionalInterfaces/functions/BiFunctionDemo.java
+++ jdk8_features/src/main/java/com/learning/functionalInterfaces/functions/BiFunctionDemo.java
@@ -7,12 +7,13 @@ import java.util.function.ToIntBiFunction;
 import java.util.function.ToLongBiFunction;
 import com.learning.model.Employee;
 import com.learning.model.dto.EmployeeDto;
+import com.learning.utils.AppConstants;
 
 public class BiFunctionDemo {
 
 	static BiFunction<Employee, Integer, Double> salaryFunction = (emp, id) -> {
-		double salary = 0d;
-		if (emp == null || id <= 0) {
+		double salary = AppConstants.DOUBLE_0;
+		if (emp == null || id <= AppConstants.ZERO) {
 			return salary;
 		}
 		if (emp.getId() == id) {
@@ -22,29 +23,29 @@ public class BiFunctionDemo {
 	};
 
 	static ToDoubleBiFunction<Integer, String> marksFunction = (id, name) -> {
-		double marks = 0d;
-		if (id <= 0 || name == null || name.isBlank() || name.isEmpty()) {
+		double marks = AppConstants.DOUBLE_0;
+		if (id <= AppConstants.ZERO || name == null || name.isBlank() || name.isEmpty()) {
 			return marks;
 		}
 
 		name = name.trim();
 
-		if (id == 1 && name.equalsIgnoreCase("ADAM")) {
-			marks = 78.5d;
-		} else if (id == 2 && name.equalsIgnoreCase("CELINA")) {
-			marks = 80.25d;
-		} else if (id == 3 && name.equalsIgnoreCase("DENIS")) {
-			marks = 90.85d;
+		if (id == AppConstants.ONE && name.equalsIgnoreCase(AppConstants.ADAM)) {
+			marks = AppConstants.DOUBLE_78_DOT_5;
+		} else if (id == AppConstants.TWO && name.equalsIgnoreCase(AppConstants.CELINA)) {
+			marks = AppConstants.DOUBLE_80_DOT_25;
+		} else if (id == AppConstants.THREE && name.equalsIgnoreCase(AppConstants.DENIS)) {
+			marks = AppConstants.DOUBLE_90_DOT_85;
 		}
 		return marks;
 	};
 
 	static ToIntBiFunction<String, String> stringLengthFunction = (s1, s2) -> {
 		if (s1 == null) {
-			s1 = "";
+			s1 = AppConstants.EMPTY;
 		}
 		if (s2 == null) {
-			s2 = "";
+			s2 = AppConstants.EMPTY;
 		}
 		s1 = s1.trim();
 		s2 = s2.trim();
@@ -56,23 +57,23 @@ public class BiFunctionDemo {
 	public static void testBiFunctions() {
 		List<Employee> employees = EmployeeDto.findEmployees();
 
-		int empId = 10;
+		int empId = AppConstants.TEN;
 		employees.stream().forEach(emp -> {
 			double salary = salaryFunction.apply(emp, empId);
-			if (salary > 0d) {
+			if (salary > AppConstants.DOUBLE_0) {
 				System.out.println("Employee Id = " + empId + " Salary = " + salary + "\n");
 			}
 		});
 
-		int id = 1;
-		String name = "Adam";
+		int id = AppConstants.ONE;
+		String name = AppConstants.ADAM;
 		double marks = marksFunction.applyAsDouble(id, name);
 		System.out.println("Candiate Id = " + id + "\nName: " + name + "\nMarks = " + marks + "\n");
 
 		int length = stringLengthFunction.applyAsInt(name, name);
 		System.out.println("String1: " + name + "\nString2: " + name + "\nLength = " + length + "\n");
 
-		int num1 = 1234567891, num2 = 312345678;
+		int num1 = AppConstants.INT_1234567891, num2 = AppConstants.INT_312345678;
 		long square = squareBiFunction.applyAsLong(num1, num2);
 		System.out.println("Number1 = " + num1 + "\nNumber2 = " + num2 + "\nSquare = " + square + "\n");
 	}
--- jdk8_features/src/main/java/com/learning/utils/AppConstants.java
+++ jdk8_features/src/main/java/com/learning/utils/AppConstants.java
@@ -17,12 +17,16 @@ public final class AppConstants {
 	public static final int FOUR = 4;
 	public static final int FIVE = 5;
 	public static final int SIX = 6;
+	public static final int TEN = 10;
 	public static final int TWELVE = 12;
 	public static final int FIFTEEN = 15;
 	public static final int EIGHTEEN = 18;
 	public static final int THIRTY_SEVEN = 37;
 	public static final int FOURTY_EIGHT = 48;
 	public static final int HUNDRED = 100;
+	public static final int INT_312345678 = 312345678;
+	public static final int INT_1234567891 = 1234567891;
+	
 
 	/** Long constants. */
 	public static final long LONG_1 = 1L;
@@ -30,7 +34,11 @@ public final class AppConstants {
 	
 	/** Double constants. */
 	public static final double DOUBLE_DOT_01113 = 0.01113d;
+	public static final double DOUBLE_0 = 0d;
 	public static final double DOUBLE_1 = 1d;
+	public static final double DOUBLE_78_DOT_5 = 78.5d;
+	public static final double DOUBLE_80_DOT_25 = 80.25;
+	public static final double DOUBLE_90_DOT_85 = 90.85d;
 	public static final double DOUBLE_1000 = 1000d;
 	public static final double DOUBLE_5000 = 5000d;
 	public static final double DOUBLE_120000 = 120000d;
@@ -38,9 +46,13 @@ public final class AppConstants {
 	public static final double DOUBLE_160000 = 160000d;
 
 	/** String constants. */
+	public static final String ADAM = "ADAM";
+	public static final String CELINA = "CELINA";
 	public static final String CHILD_THREAD = "Child-Thread";
-	public static final String CS = "CS";
 	public static final String COLON = ":";
+	public static final String CS = "CS";
+	public static final String DENIS = "DENIS";
+	public static final String EMPTY = "";
 	public static final String HDFC = "HDFC";
 	public static final String INDIA = "INDIA";
 	public static final String JOHN = "John Cena";
